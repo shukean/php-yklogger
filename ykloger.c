234063db0b6adec21b0b78ba23e5dde9de88db30
yky <yky@yky.pw>
2016-03-17 16:29:21 +0800

    fix bug: cost time & opt message include \r \n \t etc

--- ykloger.c
+++ ykloger.c
@@ -62,16 +62,16 @@ static zend_function_entry ykloger_methods[] = {
 
 static char * get_request_server(char *name, uint len TSRMLS_DC){
     zval *args = NULL;
-    
+
     if ((Z_TYPE(PG(http_globals)[TRACK_VARS_SERVER]) == IS_ARRAY || zend_is_auto_global_str(ZEND_STRL("_SERVER"))) && ((args = zend_hash_str_find_ind(Z_ARRVAL_P(&PG(http_globals)[TRACK_VARS_SERVER]), name, len)) != NULL || (args = zend_hash_str_find_ind(&EG(symbol_table), name, len)) != NULL)) {
-        
+
         if (!args) {
             return NULL;
         }
-        
+
         return estrndup(Z_STRVAL_P(args), Z_STRLEN_P(args));
     }
-    
+
     return NULL;
 }
 
@@ -79,9 +79,8 @@ static char * ykloger_array_to_string(const char **file, uint *line, zval *param
     HashTable *pmht;
     char *buf = NULL;
     size_t pos = 0, buf_length = MAX_PARAMS_STR_LEN;
-    
+
     buf = emalloc(buf_length + 1);
-    
     pmht = Z_ARRVAL_P(params);
     for (zend_hash_internal_pointer_reset(pmht); zend_hash_has_more_elements(pmht) == SUCCESS; zend_hash_move_forward(pmht)) {
         zend_string *key=NULL;
@@ -90,52 +89,85 @@ static char * ykloger_array_to_string(const char **file, uint *line, zval *param
         ulong        tmp_len;
         int          type;
         zval        *val;
-        
+
         type = zend_hash_get_current_key(pmht, &key, &idx);
-        
+
         if (type == HASH_KEY_NON_EXISTENT) {
             continue;
         }
-        
+
         if ((val = zend_hash_get_current_data(pmht)) == NULL) {
             continue;
         }
-        
+
         if (type == HASH_KEY_IS_LONG) {
             convert_to_string(val);
             //php_printf("%ld[%s] \n", idx, Z_STRVAL(tmpcopy));
             tmp_len = spprintf(&tmp, 0, "%ld[%s] ", idx, Z_STRVAL_P(val));
         }else{
-            
+			char *source, *target;
+			char *end;
+			char c;
+			zend_string *new_str = NULL;
+			size_t newlen;
+
             if (strcmp(ZSTR_VAL(key), "CUSTOM_LINE") == 0) {
                 convert_to_long(val);
                 *line = Z_LVAL_P(val);
                 continue;
             }
-            
+
             convert_to_string(val);
             if (strcmp(ZSTR_VAL(key), "CUSTOM_FILE") == 0) {
                 *file = estrndup(Z_STRVAL_P(val), Z_STRLEN_P(val));
                 continue;
             }
-            
+			new_str = zend_string_safe_alloc(4, Z_STRLEN_P(val), 0, 0);
+			for(source = (char *) Z_STRVAL_P(val), end=source + Z_STRLEN_P(val), target = ZSTR_VAL(new_str); source < end; source++) {
+				c = *source;
+				/*php_printf("%c=%d\n", c, (unsigned char)c);*/
+				if ((unsigned char) c < 32 || (unsigned char) c > 126) {
+					*target++ = '\\';
+					switch (c) {
+						case '\n': *target++ = 'n'; break;
+						case '\t': *target++ = 't'; break;
+						case '\r': *target++ = 'r'; break;
+						case '\a': *target++ = 'a'; break;
+						case '\v': *target++ = 'v'; break;
+						case '\b': *target++ = 'b'; break;
+						case '\f': *target++ = 'f'; break;
+						default :  *target++ = c;
+					}
+					continue;
+				}
+				*target++ = c;
+			}
+			*target = 0;
+			newlen = target - ZSTR_VAL(new_str);
+			if (newlen < Z_STRLEN_P(val) * 4) {
+				new_str = zend_string_truncate(new_str, newlen, 0);
+			}
+
+			/*php_printf("%s--%d", ZSTR_VAL(new_str), newlen);*/
             //php_printf("%s[%s] \n", idx, Z_STRVAL(tmpcopy));
-            tmp_len = spprintf(&tmp, 0, "%s[%s] ", ZSTR_VAL(key), Z_STRVAL_P(val));
+            tmp_len = spprintf(&tmp, 0, "%s[%s] ", ZSTR_VAL(key), ZSTR_VAL(new_str));
+
+			zend_string_release(new_str);
         }
-        
+
         if (pos + tmp_len + 1 >= buf_length) {
             buf_length = buf_length + tmp_len + MAX_PARAMS_STR_LEN;
             buf = erealloc(buf, buf_length);
         }
-        
+
         memcpy(buf + pos, tmp, tmp_len);
         efree(tmp);
-        
+
         pos += tmp_len;
     }
-    
+
     *(buf + pos) = '\0';
-    
+
     return buf;
 }
 
@@ -171,26 +203,26 @@ static char * ykloger_array_to_string(const char **file, uint *line, zval *param
 static void ykloger_flush_logs_pool(uint type){
     zval        *logfile;
     zend_string *file_name;
-    
+
     logfile = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_LOGFILE_NAME), 0);
-    
+
     if (!logfile) {
         php_error_docref(NULL, E_WARNING, "ykloger undeclared log file");
         return;
     }
-    
+
     file_name = php_format_date("YmdH", 4, time(NULL), 1);
-    
+
     if (type & LOGS_POOL_TYPE) {
         FLUSH_LOGER_POOL(logfile, "", file_name, YKLOGER_POOL_LOG);
     }
-    
+
     if (type & LOGS_POOL_WF_TYPE) {
         FLUSH_LOGER_POOL(logfile, YKLOGER_GE_WARN_PRE_NAME, file_name, YKLOGER_POOL_WF_LOG);
     }
-    
+
     zend_string_release(file_name);
-    
+
     return;
 }
 
@@ -210,30 +242,31 @@ static void ykloger_write(uint level_num, const char *level, zend_string *messag
     zval        pool_log_row;
     zval       *pool;
     uint        flush_type;
-    
+
     req_uri = get_request_server(ZEND_STRS("REQUEST_URI"));
     req_ip = get_request_server(ZEND_STRS("REMOTE_ADDR"));
     req_refer = get_request_server(ZEND_STRS("HTTP_REFERER"));
     req_date = php_format_date("Y-m-d H:i:s", 11, time(NULL), 1);
-    
+
     request_id = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_REQUEST_ID), 1);
     request_start_time = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_REQUEST_START_TIME), 1);
-    
+
     gettimeofday(&tp, NULL);
+	/*php_printf("%d", Z_LVAL_P(request_start_time));*/
     cost_usec = tp.tv_sec + tp.tv_usec / MICRO_IN_SEC - Z_LVAL_P(request_start_time);
-    
+
     params2str = params ? ykloger_array_to_string(&code_filename, &code_line, params) : NULL;
-    
+
     if (!code_filename || code_line == 0) {
         code_filename = zend_get_executed_filename(TSRMLS_C);
         code_line = zend_get_executed_lineno(TSRMLS_C);
     }
     php_pid = getpid();
-    
+
     log_row_len = spprintf(&log_row, 0, "%-5s %s,%d [%s:%d] pid[%d] reqip[%s] uri[%s] refer[%s] cost[%f] errno[%d] reqid[%s] %s%s\n", level, ZSTR_VAL(req_date), tp.tv_usec, code_filename, code_line, php_pid, req_ip ? req_ip : "", req_uri ? req_uri : "", req_refer ? req_refer : "", cost_usec, loger_errno, Z_STRVAL_P(request_id), params2str ? params2str : "", ZSTR_VAL(message));
-    
+
     //php_printf("%s", log_row);
-    
+
     if (NULL != req_uri) {
         efree(req_uri);
     }
@@ -247,9 +280,9 @@ static void ykloger_write(uint level_num, const char *level, zend_string *messag
     if (NULL != params2str) {
         efree(params2str);
     }
-    
+
     ZVAL_STRINGL(&pool_log_row, log_row, log_row_len);
-    
+
     if (level_num >= YKLOGER_LEVEL_WARN) {
         pool = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_POOL_WF_LOG), 1);
         flush_type = LOGS_POOL_WF_TYPE;
@@ -257,18 +290,18 @@ static void ykloger_write(uint level_num, const char *level, zend_string *messag
         pool = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_POOL_LOG), 1);
         flush_type = LOGS_POOL_TYPE;
     }
-    
+
     //php_printf("%s", Z_STRVAL(pool_log_row));
     Z_ADDREF(pool_log_row);
     zend_hash_next_index_insert(Z_ARRVAL_P(pool), &pool_log_row);
-    
+
     if (zend_hash_num_elements(Z_ARRVAL_P(pool)) >= YKLOGER_G(auto_flush_num)) {
         ykloger_flush_logs_pool(flush_type);
     }
-    
+
     zval_ptr_dtor(&pool_log_row);
     efree(log_row);
-    
+
     return;
 }
 
@@ -279,17 +312,17 @@ PHP_METHOD(ykloger, init){
     zend_string *logfile_key, *loglevel_key;
     zval *is_inited = NULL;
     zval log_pool, log_wf_pool;
-    
+
     is_inited = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_LOG_INITED), 1);
     if (Z_LVAL_P(is_inited)) {
         php_error_docref(NULL, E_NOTICE, "ykloger is all ready inited");
         RETURN_NULL();
     }
-    
+
     if (zend_parse_parameters(ZEND_NUM_ARGS(), "a", &conf) == FAILURE) {
         RETURN_FALSE;
     }
-    
+
     logfile_key = zend_string_init(ZEND_STRL(YKLOGER_LOGFILE_NAME), 0);
     logfile = zend_hash_find(Z_ARRVAL_P(conf), logfile_key);
     if (logfile == NULL) {
@@ -297,7 +330,7 @@ PHP_METHOD(ykloger, init){
         php_error_docref(NULL, E_ERROR, "ykloger init undeclared %s", YKLOGER_LOGFILE_NAME);
         RETURN_FALSE;
     }
-    
+
     loglevel_key = zend_string_init(ZEND_STRL(YKLOGER_LOGLEVEL_NAME), 0);
     loglevel = zend_hash_find(Z_ARRVAL_P(conf), loglevel_key);
     if (loglevel == NULL) {
@@ -306,7 +339,7 @@ PHP_METHOD(ykloger, init){
         php_error_docref(NULL, E_ERROR, "ykloger init undeclared %s", YKLOGER_LOGLEVEL_NAME);
         RETURN_FALSE;
     }
-    
+
     if (Z_TYPE_INFO_P(loglevel) == IS_STRING) {
         char *level_str;
         level_str = estrndup(Z_STRVAL_P(loglevel), Z_STRLEN_P(loglevel));
@@ -322,6 +355,7 @@ PHP_METHOD(ykloger, init){
         }else if (strcmp(level_str, YKLOGER_LEVEL_FATAL_NAME) == 0) {
             level_val = YKLOGER_LEVEL_FATAL;
         }else{
+			level_val = 0;
             php_error_docref(NULL, E_ERROR, "ykloger init invalid logLevel name %s", level_str);
         }
         efree(level_str);
@@ -329,22 +363,22 @@ PHP_METHOD(ykloger, init){
         convert_to_long_ex(loglevel);
         level_val = Z_LVAL_P(loglevel);
     }
-    
+
     zend_update_static_property(ykloger_ce, ZEND_STRL(YKLOGER_LOGFILE_NAME), logfile);
     zend_update_static_property_long(ykloger_ce, ZEND_STRL(YKLOGER_LOGLEVEL_NAME), level_val);
-    
+
     ZVAL_LONG(is_inited, 1);
     zend_update_static_property(ykloger_ce, ZEND_STRL(YKLOGER_LOG_INITED), is_inited);
-    
+
     array_init_size(&log_pool, 30);
     array_init_size(&log_wf_pool, 10);
-    
+
     zend_update_static_property(ykloger_ce, ZEND_STRL(YKLOGER_POOL_LOG), &log_pool);
     zend_update_static_property(ykloger_ce, ZEND_STRL(YKLOGER_POOL_WF_LOG), &log_wf_pool);
-    
+
     zend_string_release(logfile_key);
     zend_string_release(loglevel_key);
-    
+
     RETURN_TRUE;
 }
 
@@ -380,43 +414,43 @@ do{ \
 
 
 PHP_METHOD(ykloger, debug){
-    
+
     LOGER_METHODS(YKLOGER_LEVEL_DEBUG, YKLOGER_LEVEL_DEBUG_NAME);
 }
 
 PHP_METHOD(ykloger, info){
-    
+
     LOGER_METHODS(YKLOGER_LEVEL_INFO, YKLOGER_LEVEL_INFO_NAME);
 }
 
 PHP_METHOD(ykloger, warn){
-    
+
     LOGER_METHODS(YKLOGER_LEVEL_WARN, YKLOGER_LEVEL_WARN_NAME);
 }
 
 PHP_METHOD(ykloger, error){
-    
+
     LOGER_METHODS(YKLOGER_LEVEL_ERROR, YKLOGER_LEVEL_ERROR_NAME);
 }
 
 PHP_METHOD(ykloger, fatal){
-    
+
     LOGER_METHODS(YKLOGER_LEVEL_FATAL, YKLOGER_LEVEL_FATAL_NAME);
 }
 
 PHP_METHOD(ykloger, resetRequestTime){
     double request_time = 0;
-    
+
     if (zend_parse_parameters(ZEND_NUM_ARGS(), "|l", &request_time) == FAILURE) {
         return;
     }
     if (!request_time) {
         struct timeval tp = {0};
-        
+
         gettimeofday(&tp, NULL);
         request_time = tp.tv_sec + tp.tv_usec / MICRO_IN_SEC;
     }
-    
+
     zend_update_static_property_long(ykloger_ce, ZEND_STRL(YKLOGER_REQUEST_START_TIME), request_time);
     RETURN_TRUE;
 }
@@ -446,12 +480,12 @@ PHP_MINIT_FUNCTION(ykloger)
     REGISTER_LONG_CONSTANT("YKLOGER_LEVEL_WARN",    YKLOGER_LEVEL_WARN,     CONST_CS | CONST_PERSISTENT);
     REGISTER_LONG_CONSTANT("YKLOGER_LEVEL_ERROR",   YKLOGER_LEVEL_ERROR,    CONST_CS | CONST_PERSISTENT);
     REGISTER_LONG_CONSTANT("YKLOGER_LEVEL_FATAL",   YKLOGER_LEVEL_FATAL,    CONST_CS | CONST_PERSISTENT);
-    
+
     zend_class_entry ce;
-    
+
     INIT_CLASS_ENTRY(ce, "ykloger", ykloger_methods);
     ykloger_ce = zend_register_internal_class(&ce);
-    
+
     zend_declare_property_null(ykloger_ce, ZEND_STRL(YKLOGER_LOG_INITED), ZEND_ACC_STATIC | ZEND_ACC_PRIVATE);
     zend_declare_property_null(ykloger_ce, ZEND_STRL(YKLOGER_LOGFILE_NAME), ZEND_ACC_STATIC | ZEND_ACC_PRIVATE);
     zend_declare_property_null(ykloger_ce, ZEND_STRL(YKLOGER_LOGLEVEL_NAME), ZEND_ACC_STATIC | ZEND_ACC_PRIVATE);
@@ -459,7 +493,7 @@ PHP_MINIT_FUNCTION(ykloger)
     zend_declare_property_null(ykloger_ce, ZEND_STRL(YKLOGER_REQUEST_START_TIME), ZEND_ACC_STATIC | ZEND_ACC_PRIVATE);
     zend_declare_property_null(ykloger_ce, ZEND_STRL(YKLOGER_POOL_LOG), ZEND_ACC_STATIC | ZEND_ACC_PRIVATE);
     zend_declare_property_null(ykloger_ce, ZEND_STRL(YKLOGER_POOL_WF_LOG), ZEND_ACC_STATIC | ZEND_ACC_PRIVATE);
-    
+
     zend_register_class_alias(YKLOGER_ALIAS_BEEQUICK_NAME, ykloger_ce);
 
 	return SUCCESS;
@@ -471,7 +505,7 @@ PHP_MINIT_FUNCTION(ykloger)
 PHP_MSHUTDOWN_FUNCTION(ykloger)
 {
     UNREGISTER_INI_ENTRIES();
-    
+
     return SUCCESS;
 }
 /* }}} */
@@ -481,11 +515,11 @@ PHP_MSHUTDOWN_FUNCTION(ykloger)
  */
 PHP_RINIT_FUNCTION(ykloger)
 {
-    
+
 #if defined(COMPILE_DL_YKLOGER) && defined(ZTS)
 	ZEND_TSRMLS_CACHE_UPDATE();
 #endif
-    
+
     zval request_id;
     zval request_time;
     char request_id_str[33];
@@ -495,28 +529,28 @@ PHP_RINIT_FUNCTION(ykloger)
     unsigned char digest[16];
     char *arg;
     uint arg_len;
-    
+
     gettimeofday(&tp, NULL);
     request_start_time = tp.tv_sec + tp.tv_usec / MICRO_IN_SEC;
-    
+
     request_id_str[0] = '\0';
     arg_len = spprintf(&arg, 0, "%s%08x%05x%.8F", "ykloger", (int)tp.tv_sec, (int)(tp.tv_usec % 0x100000), php_combined_lcg() * 10);
-    
+
     PHP_MD5Init(&context);
     PHP_MD5Update(&context, arg, arg_len);
     PHP_MD5Final(digest, &context);
     make_digest_ex(request_id_str, digest, 16);
-    
+
     ZVAL_STRING(&request_id, request_id_str);
-    ZVAL_DOUBLE(&request_time, request_start_time);
-    
+    ZVAL_LONG(&request_time, request_start_time);
+
     zend_update_static_property(ykloger_ce, ZEND_STRL(YKLOGER_REQUEST_ID), &request_id);
     zend_update_static_property(ykloger_ce, ZEND_STRL(YKLOGER_REQUEST_START_TIME), &request_time);
     zend_update_static_property_long(ykloger_ce, ZEND_STRL(YKLOGER_LOG_INITED), 0);
-    
+
     efree(arg);
     zval_ptr_dtor(&request_id);
-    
+
 	return SUCCESS;
 }
 /* }}} */
@@ -527,20 +561,20 @@ PHP_RINIT_FUNCTION(ykloger)
 PHP_RSHUTDOWN_FUNCTION(ykloger)
 {
     zval *is_inited;
-    
+
     is_inited = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_LOG_INITED), 1);
-    
+
     if (Z_LVAL_P(is_inited)) {
         zval *log_pool, *log_wf_pool;
 
         ykloger_flush_logs_pool(LOGS_POOL_TYPE | LOGS_POOL_WF_TYPE);
-        
+
         log_pool = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_POOL_LOG), 0);
         zval_ptr_dtor(log_pool);
         log_wf_pool = zend_read_static_property(ykloger_ce, ZEND_STRL(YKLOGER_POOL_WF_LOG), 0);
         zval_ptr_dtor(log_wf_pool);
     }
-    
+
 	return SUCCESS;
 }
 /* }}} */
@@ -554,7 +588,7 @@ PHP_MINFO_FUNCTION(ykloger)
 	php_info_print_table_end();
 
 	DISPLAY_INI_ENTRIES();
-	
+
 }
 /* }}} */
 
